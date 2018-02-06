fd6e217752e0956cca2375c0ab836ccd1bec4f54
hao.zhang <hao.zhang@flaginfo.com.cn>
2018-02-06 17:05:44 +0800

    上传图片使用multe处理

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,17 @@ var bodyParser = require('body-parser');
 var session = require('express-session')
 //json类型的body数据,如果不用改模块，req.body为undefined
 app.use(bodyParser.json());
-
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+console.log(upload, 'up')
 //string类型
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
@@ -21,4 +31,16 @@ var addNews = require('./addNews')
 app.use('/login', login)  //设置路由
 app.use('/mainList', mainList)  //设置路由
 app.use('/addNews', addNews)  //设置路由
+app.post('/upload',upload.single('avater'), function(req, res, next) {
+  console.log(req);
+  console.log(req.file, 1);
+  if (req.file) {
+    res.send({
+      head:{
+        resultCode: 200
+      },
+      body:req.file.path
+    })
+  }
+});
 module.exports = app
